b90ed14cf233fd6463b4e86c26db768686998ab2
T__R__P__53 <tyler_1701@hotmail.com>
2021-05-05 08:27:05 -0400

    Updates

---
+++ precache-manifest.aabef7beca77074fb67aedaec3ebc7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c9a3a855213890a1761de44d7f5ec",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "4367c9c99f24d58f589f",
+    "url": "/Sorting-Visualizer/static/css/main.b6b37f6a.chunk.css"
+  },
+  {
+    "revision": "fb4118144304417e01a7",
+    "url": "/Sorting-Visualizer/static/js/2.30d8ebc7.chunk.js"
+  },
+  {
+    "revision": "4367c9c99f24d58f589f",
+    "url": "/Sorting-Visualizer/static/js/main.c55fe303.chunk.js"
+  },
+  {
+    "revision": "b7b9686e25c48a831049",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.b2f9a7a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Visualizer/precache-manifest.43dbdcd6c4dba2f08456713bb0fde24e.js"
+  "/Sorting-Visualizer/precache-manifest.aabef7beca77074fb67aedaec3ebc7cf.js"
 );
 
 self.addEventListener('message', (event) => {
